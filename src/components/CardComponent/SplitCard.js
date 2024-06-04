d4ef4fc3c321dfebea9fce84d59bb6b98fe72b50
nisha8291 <ynisha509@gmail.com>
2024-06-04 17:04:26 +0530

    NY(splitcard)

--- src/components/CardComponent/SplitCard.js
+++ src/components/CardComponent/SplitCard.js
@@ -1,99 +1,150 @@
-import React, { Component } from 'react';
-import 'tailwindcss/tailwind.css'; // Ensure Tailwind CSS is imported
+import React, { Component } from "react";
+import "./SplitCard.scss";
+import "tailwindcss/tailwind.css"; // Ensure Tailwind CSS is imported
+import Aos from "aos";
+import webDevelopment from "../../assets/images/web development.png";
+import javascript from "../../assets/images/java-script.png"
+import reactlogo from "../../assets/images/react.png"
+import nodelogo from "../../assets/images/node.png"
+import javalogo from "../../assets/images/java.png"
+import kotline from "../../assets/images/Kotlin_Icon.png"
+import flutter from "../../assets/images/flutter.png"
+import enterpriselogo from "../../assets/images/computerlogo.png"
+import digital from "../../assets/images/digital.png"
+import solution from "../../assets/images/solution.png"
 
 export default class SplitCard extends Component {
-    render() {
-        return (
-            <div className="flex flex-col items-center min-h-screen bg-gray-100">
-              <div className="mx-auto w-11/12 mt-4 relative">
-              {/* <div className="absolute left-10 top-20 z-10 bg-blue-500 text-white text-2xl font-bold flex items-center justify-center shadow-lg h-72 lg:h-72 w-72 lg:w-1/4 p-6 rounded-xl hover:-translate-y-6 transition-all duration-500">
+  render() {
+    return (
+      <div className="flex flex-col items-center min-h-screen bg-gray-100">
+        {/* style={{ backgroundImage: `url(${webDevelopment})` }} */}
+        <div
+          data-aos="fade-up"
+          data-aos-duration="4000"
+          className="mx-auto w-10/12 mt-4 relative containt"
+        >
+          {/* <div className="absolute left-10 top-20 z-10 bg-blue-500 text-white text-2xl font-bold flex items-center justify-center shadow-lg h-72 lg:h-72 w-72 lg:w-1/4 p-6 rounded-xl hover:-translate-y-6 transition-all duration-500">
         Web Development
     </div> */}
-    <div className="absolute left-10 top-20 z-10 bg-blue-500 bg-cover bg-center text-white text-2xl font-bold flex items-center justify-center shadow-lg h-72 lg:h-72 w-72 lg:w-1/4 p-6 rounded-xl hover:-translate-y-6 transition-all duration-500" style={{ backgroundImage: "url('/path-to-your-image.jpg')" }}>
-    <div className="bg-blue-500 bg-opacity-75 p-6 rounded-xl">
-        Web Development
-    </div>
-</div>
 
-
-    <div className="bg-white w-[90%]  relative left-20 shadow-2xl p-6 rounded-xl mb-5 transform transition-all duration-300">
-        <div className="flex flex-col items-end ">
-            <div className="mt-8 text-center lg:text-left">
-                {/* <h2 className="text-lg text-gray-600 mb-4">Web Development</h2> */}
-                <p className="text-xl font-bold text-gray-900 mb-4">Deploy leading edge web apps that get you predictability closer to innovation.</p>
-                {/* <hr className="border-t-2 bg-gray-200" /> */}
-                <ul className="space-y-2 text-gray-700"type='circle'>
-                <li>Strategic technical consulting and solution architecture</li>
-                    <li>Research-driven implementation of cutting-edge technologies</li>
-                    <li>Exploration of emerging tech trends and experimentation</li>
-                    <li>Continuous refinement of existing tech stacks and team skill development</li>
-                    <li>Intuitive design for seamless user interactions</li>
-                    <li>Compelling content creation and storytelling</li>
-                    <li>Rigorous code reviews and optimization</li>
-                    <li>End-to-end development and DevOps integration</li>
-                    <li>Comprehensive testing and quality assurance protocols</li>
-                    <li>Reliable delivery and ongoing maintenance support</li>
-
-                </ul>
+          <div
+            
+            className="absolute   left-10 top-10 z-10  bg-cover bg-center text-white text-2xl font-bold flex  customborder bg-blue-500 justify-center shadow-lg h-72 lg:h-72 w-72 lg:w-1/4 hover:-translate-y-6 rounded transition-all duration-500"
+            web Development 
+          >
+            <div
+              className=" bg-opacity-75  rounded  "
+              
+            >
+            
             </div>
-        </div>
-    </div>
-</div>
+          </div>
 
-<div className="mx-auto w-11/12  relative">
-              <div className="absolute right-1 top-10 z-10 bg-blue-500  text-white text-2xl font-bold flex items-center  text-center justify-center shadow-lg h-72 lg:h-72 w-72 lg:w-1/4 p-6 rounded-xl hover:-translate-y-6 transition-all duration-500">
-        Mobile App Development
-    </div>
-
-    <div className="bg-white w-[90%]  relative left-20 shadow-2xl p-6 rounded-xl  transform transition-all duration-300">
-        <div className="flex flex-col items-start ">
-            <div className="mt-8 text-center lg:text-left">
+          <div className="bg-white w-[90%]  relative left-20 shadow-2xl  p-6 rounded-xl mb-5 transform transition-all duration-300 border hover:border-blue-700">
+            <div className="flex  h-80 pl-72 flex-col items-center     ">
+              <div className=" h-full  flex justify-between w-full ">
                 {/* <h2 className="text-lg text-gray-600 mb-4">Web Development</h2> */}
-                <p className="text-xl font-bold text-gray-900 mb-4">Deploy leading edge web apps that get you predictability closer to innovation.</p>
-                {/* <hr className="border-t-2 bg-gray-200" /> */}
-                <ul className="space-y-2 text-gray-700">
-                <li>Strategic consulting for technical decisions and solution architecture</li>
-                    <li>Research-driven implementation of innovative ideas</li>
-                    <li>Utilization of leading-edge frameworks for intuitive design</li>
-                    <li>Regular code reviews for quality assurance and optimization</li>
-                    <li>Integrated development and DevOps practices for efficient workflows</li>
-                    <li>Thorough testing and quality assurance procedures for reliability</li>
-                    <li>Efficient delivery and ongoing maintenance support</li>
-                    <li>Robust monitoring systems and proactive alert mechanisms</li>
 
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
- {/* <div className=" mx-auto w-11/12  mb-10 bg-white shadow-lg p-6 rounded-xl transform transition-all duration-300">
- 
-  <div className="flex flex-col items-center ">
-  <div className="mx-auto w-11/12 mt-10 relative ">
-  <div className="relative h-72 w-full bg-blue-500 text-white text-2xl font-bold flex items-center justify-center rounded-xl shadow-lg lg:h-96">
-  
-     Mobile App Development
-    </div>
-                        <div className="mt-8 text-center lg:text-left ">
-                            <h2 className="text-lg text-gray-600 mb-4">Mobile App Development</h2>
-                            <p className="text-xl font-bold text-gray-900 mb-4">Mobile Applications, both on Native & Hybrid frameworks for handheld software development.</p>
-                            <hr className="border-t-2 border-gray-300 mb-6" />
-                            <ul className="space-y-2 text-gray-700">
-                                <li>Consulting Technical Decision & Solution Architecture</li>
-                                <li>Research & Implementation</li>
-                                <li>Design using leading edge frameworks</li>
-                                <li>Frequent code reviews</li>
-                                <li>Development & Dev-Ops enablement</li>
-                                <li>Testing & Quality Assurances</li>
-                                <li>Delivery & Maintenance</li>
-                                <li>Monitoring & Alerts</li>
-                            </ul>
-                        </div>
-                    </div>
+                {/* <hr className="border-t-2 bg-gray-200" /> */}
+                <div className="pl-2 flex flex-col justify-between ">
+                  <h4 className="heading smallcontaint">Web Development</h4>
+                  <p className="text-gray-700 font ">
+                    We offer custom web design and development, creating
+                    responsive and user-friendly interfaces tailored to your
+                    needs. We build secure and scalable architectures, ensuring
+                    your website is robust and future-proof. Regular updates and
+                    maintenance are provided to keep your site running smoothly,
+                    accompanied by comprehensive testing for quality assurance.
+                    
+                  </p>
+                  <div className="flex  justify-end items-end gap-4">
+                    <img src={javascript} className="h-10 " />
+                    <img src={reactlogo} className="h-10"/>
+                    <img src={nodelogo} className="h-10"/>
+                  </div>
                 </div>
-            </div> */}
-            </div> 
-        );
-    }
-}
+              </div>
+            </div>
+          </div>
+        </div>
 
+        <div
+          data-aos="fade-up"
+          data-aos-duration="4000"
+          className="mx-auto w-10/12 relative containt "
+        >
+          <div className="absolute right-[-1.75rem] top-10 z-10 bg-blue-500 customborder text-white text-2xl font-bold flex items-center  text-center justify-center shadow-lg h-72 lg:h-72 w-72 lg:w-1/4 p-6 rounded hover:-translate-y-6 transition-all duration-500"></div>
+           
+          <div className="bg-white w-[90%]  relative left-20  shadow-2xl p-6 rounded-2xl  transform transition-all duration-300 border hover:border-blue-700">
+            <div className="flex flex-col h-80 items-start">
+              
+              <h4 className="heading smallcontaint pl-6">Mobile App Development</h4>
+              <p className="text-gray-700 h-72 font p-6 w-[74%] ">
+                We provide custom mobile app design and development, ensuring
+                user-friendly interfaces and intuitive UX.
+                Our solutions offer cross-platform compatibility and regular updates and
+                maintenance. We implement robust security measures and seamless
+                integration with backend systems.
+              </p>
+              <div className="flex justify-end items-end gap-4 ">
+                <img src={javalogo} className="h-12 "/>
+                <img src={kotline} className="h-9 image "/>
+                <img src={flutter} className="h-9 image "/>
+                
+                
+
+                {/* <img src={} className="h-12"/> */}
+              </div>
+              {/* </div> */}
+            </div>
+          </div>
+          
+        </div>
+
+        <div
+          data-aos="fade-up"
+          data-aos-duration="4000"
+          className="mx-auto w-10/12  relative containt"
+        >
+          <div className="absolute left-10 top-16 z-10 customborder bg-blue-500 bg-cover bg-center  text-white text-2xl font-bold flex items-center justify-center shadow-lg h-72 lg:h-72 w-72 lg:w-1/4 p-6 rounded hover:-translate-y-6 transition-all duration-500">
+            <div className="bg-blue-500  bg-opacity-75 p-6 rounded"></div>
+          </div>
+
+          <div className="bg-white  w-[90%] h-96 relative left-20 shadow-2xl  p-4 rounded-xl mb-5 mt-5 transform transition-all duration-300 border hover:border-blue-700">
+            <div className="flex flex-col items-end ">
+              <div className="mb-10 ">
+                {/* <h2 className="text-lg text-gray-600 mb-4">Web Development</h2> */}
+
+                {/* <hr className="border-t-2 bg-gray-200" /> */}
+                <h4 className=" text-center heading smallcontaint pl-60 pr-40 ">
+                  
+                  Enterprise Application Solutions
+                </h4>
+                <div className="pl-2  flex justify-center   ">
+                  <p className="text-gray-700 font h-72 p-6 pl-80  w-[100%] line-break:anywhere">
+                    We provide strategic consulting for enterprise application
+                    architecture and the implementation of innovative enterprise
+                    solutions. Our approach includes the utilization of scalable
+                    and intuitive design frameworks, regular code reviews for
+                    quality and optimization, and integrated DevOps for
+                    efficient workflows. 
+                  </p>
+                  <div className="flex justify-end items-end gap-4 ">
+                <img src={enterpriselogo} className="h-10  "/>
+                <img src={digital} className="h-10 "/>
+                <img src={solution} className="h-10  "/>
+
+                
+                
+
+                {/* <img src={} className="h-12"/> */}
+              </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -8,12 +8,13 @@ import {
   FaInstagram,
   FaLinkedin,
   FaTwitter,
+  FaBlackTie,
 } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="pg-footer">
-      <footer className="footer  " style={{ backgroundImage: "radial-gradient(circle at bottom right  , rgba(86,255,248,1) 1%, rgba(13,11,136,1) 53%)" }}>
+      <footer className="footer  " style={{  backgroundColor: "rgb(0, 96, 181)" }}>
         <div className="footer-content container mx-auto h-96 ">
           <div className="row flex flex-wrap">
             <div className="footer-col w-full sm:w-1/2 lg:w-1/4 px-4 mb-8 sm:mb-0 mt-10">
@@ -25,30 +26,31 @@ const Footer = () => {
               <div className="footer-menu">
                 <h4 className="footer-menu-name text-xl text-white capitalize mb-8 relative font-medium">
                   Get Started
-                  {/* <span className="block w-12 h-0.5 bg-pink-500 absolute left-0 -bottom-2"></span> */}
-                                    <span
+                  <span className="block w-12 h-0.5 bg-black absolute left-0 -bottom-2"></span>
+                                    {/* <span
                     className="block w-12 h-0.5 absolute left-0 -bottom-2"
                     style={{
                       backgroundImage:
                         // "linear-gradient(109.5deg, rgba(13, 11, 110, 1) 10%, rgba(86, 255, 248, 1) 90%, rgba(135, 206, 250, 1) 90.4%)"
                         "linear-gradient(109.5deg, rgba(13, 11, 170, 1) 9.4%, rgba(135, 206, 250, 1) 50%, rgba(86, 255, 248, 1) 78.4%)",
-                        
+                       
+
                     }}
-                  ></span>
+                  ></span> */}
                 </h4>
                 <ul className="footer-menu-list space-y-3">
                   <li>
-                    <a href="/" className="text-base text-gray-400 hover:text-white	hover:pl-2 transition-all">
+                    <a href="/" className="text-base hover:text-gray-400 text-white	hover:pl-2 transition-all">
                       Home
                     </a>
                   </li>
                   <li>
-                    <a href="/technology" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/technology" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Technology
                     </a>
                   </li>
                   <li>
-                    <a href="/recruitment" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/recruitment" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Recruitment
                     </a>
                   </li>
@@ -59,8 +61,8 @@ const Footer = () => {
               <div className="footer-menu">
                 <h4 className="footer-menu-name text-xl text-white capitalize mb-8  relative font-medium">
                   Company
-                  {/* <span className="block w-12 h-0.5 bg-pink-500 absolute left-0 -bottom-2"></span> */}
-                  <span
+                  <span className="block w-12 h-0.5 bg-black absolute left-0 -bottom-2"></span>
+                  {/* <span
                     className="block w-12 h-0.5 absolute left-0 -bottom-2"
                     style={{
                       backgroundImage:
@@ -68,26 +70,26 @@ const Footer = () => {
                         "linear-gradient(109.5deg, rgba(13, 11, 170, 1) 9.4%, rgba(135, 206, 250, 1) 50%, rgba(86, 255, 248, 1) 78.4%)",
                         
                     }}
-                  ></span>
+                  ></span> */}
                 </h4>
                 <ul className="footer-menu-list space-y-3">
                   <li>
-                    <a href="/Contact" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/Contact" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Contact
                     </a>
                   </li>
                   <li>
-                    <a href="/Careers" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/Careers" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Careers
                     </a>
                   </li>
                   <li>
-                    <a href="/technology-clients" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/technology-clients" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Technology Clients
                     </a>
                   </li>
                   <li>
-                    <a href="/recruitment-clients" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/recruitment-clients" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Recruitment Clients
                     </a>
                   </li>
@@ -98,8 +100,8 @@ const Footer = () => {
               <div className="footer-menu">
                 <h4 className="footer-menu-name text-xl text-white capitalize mb-8 mt-20  relative font-medium">
                   Quick Links
-                  {/* <span className="block w-12 h-0.5 bg-pink-500 absolute left-0 -bottom-2"></span> */}
-                  <span
+                  <span className="block w-12 h-0.5 bg-black absolute left-0 -bottom-2"></span>
+                  {/* <span
                     className="block w-12 h-0.5 absolute left-0 -bottom-2"
                     style={{
                       backgroundImage:
@@ -107,16 +109,16 @@ const Footer = () => {
                         "linear-gradient(109.5deg, rgba(13, 11, 170, 1) 9.4%, rgba(135, 206, 250, 1) 50%, rgba(86, 255, 248, 1) 78.4%)",
                         
                     }}
-                  ></span>
+                  ></span> */}
                 </h4>
                 <ul className="footer-menu-list space-y-3">
                   <li>
-                    <a href="/technology-services" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/technology-services" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Technology Services
                     </a>
                   </li>
                   <li>
-                    <a href="/recruitment-services" className="text-base text-gray-400 hover:text-white hover:pl-2 transition-all">
+                    <a href="/recruitment-services" className="text-base hover:text-gray-400 text-white hover:pl-2 transition-all">
                       Recruitment Services
                     </a>
                   </li>
@@ -127,19 +129,19 @@ const Footer = () => {
               <div className="footer-call-to-action  ">
                 <h4 className="footer-call-to-action-title  text-xl   text-white capitalize mb-4 relative font-medium ">
                   You Call Us
-                  {/* <span className="block w-12 h-0.5 bg-pink-500 absolute left-0 -bottom-2"></span> */}
-                  <span
+                  <span className="block w-12 h-0.5 bg-black absolute left-0 -bottom-2"></span>
+                  {/* <span
                     className="block w-12 h-0.5 absolute left-0 -bottom-2"
                     style={{
-                      backgroundImage:
+                      backgroundcolour:
                         // "linear-gradient(109.5deg, rgba(13, 11, 110, 1) 10%, rgba(86, 255, 248, 1) 90%, rgba(135, 206, 250, 1) 90.4%)"
-                        "linear-gradient(109.5deg, rgba(13, 11, 170, 1) 9.4%, rgba(135, 206, 250, 1) 50%, rgba(86, 255, 248, 1) 78.4%)",
+                        // "linear-gradient(109.5deg, rgba(13, 11, 170, 1) 9.4%, rgba(135, 206, 250, 1) 50%, rgba(86, 255, 248, 1) 78.4%)",
                         
                     }}
-                  ></span>
+                  ></span> */}
                 </h4>
                 <p className="footer-call-to-action-link-wrapper ">
-                  <a href="tel:0124-64XXXX" className="text-base text-gray-400 hover:text-white hover:pl-2  transition-all">
+                  <a href="tel:0124-64XXXX" className="text-base hover:text-gray-400 text-white hover:pl-2  transition-all">
                     (+91) 022 65510355
                   </a>
                 </p>
@@ -147,8 +149,8 @@ const Footer = () => {
               <div className="footer-call-to-action ">
                 <h4 className="footer-call-to-action-title text-xl text-white capitalize mb-4 relative font-medium">
                   Address
-                  {/* <span className="block w-12 h-0.5 bg-pink-500 absolute left-0 -bottom-2"></span> */}
-                  <span
+                  <span className="block w-12 h-0.5 bg-black absolute left-0 -bottom-2"></span>
+                  {/* <span
                     className="block w-12 h-0.5 absolute left-0 -bottom-2"
                     style={{
                       backgroundImage:
@@ -156,9 +158,9 @@ const Footer = () => {
                         "linear-gradient(109.5deg, rgba(13, 11, 170, 1) 9.4%, rgba(135, 206, 250, 1) 50%, rgba(86, 255, 248, 1) 78.4%)",
                         
                     }}
-                  ></span>
+                  ></span> */}
                 </h4>
-                <p className="text-base text-gray-400">
+                <p className="text-base text-white">
                   912 | 1st Floor | Building No 9 | Solitaire Corporate Park | Andheri Kurla Road | Chakala | Andheri (East) | Mumbai - 400 093
                 </p>
               </div>
